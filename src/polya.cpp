f2a3cb825446752a35f6d1ef3f69098a2c85ff26
Sam Gwydir <sgwydir@groupon.com>
2017-03-24 14:01:28 -0500

    final update

--- src/polya.cpp
+++ src/polya.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <cstdio>
 #include <gmpxx.h>
+// #include <NTL/BasicThreadPool.h>
 #include <NTL/ZZ.h>
 #include <NTL/ZZ_p.h>
+
+#define PI 3.14159265358
 
 // From NTL
 int Legendre(const NTL::ZZ& aa, const NTL::ZZ& nn)
@@ -32,6 +36,7 @@ int Legendre(const NTL::ZZ& aa, const NTL::ZZ& nn)
 
 int main(int argc, char *argv[])
 {
+  // NTL::SetNumThreads(4);
 
   // set the number of bits in our modulus using first argument of program
   int numbit = atoi(argv[1]);
@@ -40,20 +45,66 @@ int main(int argc, char *argv[])
   NTL::ZZ p = NTL::GenPrime_ZZ(numbit);
   NTL::ZZ_p::init(p);
 
-  // set our range using second and third argument of program
-  NTL::ZZ m = NTL::ZZ(0);
-  NTL::ZZ n = p -1;
+  // set our range to 512 numbers
+  // NTL::ZZ n = NTL::ZZ(32768);
+  // NTL::ZZ n = p-1;
+  // Generate a random starting point 
+  // std::srand(std::time(0)); // use current time as seed for random generator
+  // NTL::ZZ m = (NTL::RandomBits_ZZ(std::rand())) % p; // use standard random function as seed for "big int" random number
+  // NTL::ZZ m = NTL::ZZ(0);
 
   // optionally on command line
-  // NTL::ZZ m = NTL::ZZ(atoi(argv[2]));
-  // NTL::ZZ n = NTL::ZZ(atoi(argv[3]));
+  NTL::ZZ m = NTL::ZZ(atoi(argv[2]));
+  NTL::ZZ n = NTL::ZZ(atoi(argv[3]));
+
+  std::cerr << "Modulus: "<< p << std::endl;
+  std::cerr << "Length of Interval (N): "<< n << std::endl;
+  std::cerr << "Start of Interval (M): "<< m << std::endl;
+  std::cerr << "Length of char: "<< sizeof(char) << std::endl;
+
+  unsigned char c = 0;
+  int j;
+  unsigned int k = 0;
+  NTL::ZZ sum = NTL::ZZ(0);
 
   for (NTL::ZZ i = NTL::ZZ(m + 1); i <= (n+m); ++i) {
-    // std::cout << i << "\t" << NTL::Jacobi(i, p) << std::endl;
-    // std::cout << NTL::Jacobi(i, p) << std::endl;
-    unsigned int j = Legendre(i, p);
-    if (j == (unsigned int)-1) std::cout << "0";
-    else std::cout << "1";
+    // // std::cout << i << "\t" << NTL::Jacobi(i, p) << std::endl;
+    // // std::cout << NTL::Jacobi(i, p) << std::endl;
+    // int j = Legendre(i, p);
+    // // if (j == -1) std::cout << '0';
+    // // else std::cout << '1';
+
+    // if (j == -1) j = 0;
+    // std::cout << j;
+
+    // calculate legendre
+    j = Legendre(i, p);
+    sum += j;
+    // if you get 1, add 1
+    // else j is -1. Add nothing, shifting in a 0 as our interpretation of -1
+    if (j == 1) {
+      c |= 1;
+    }
+
+    // if our char if full, print it, else shift it for the next bit
+    if (k % 8 == 0) {
+      std::cout << c;
+      c = 0;
+    } else {
+      c <<= 1;
+    }
+    k++;
+
   }
+  std::cerr << "Sum: " << NTL::abs(sum) << std::endl;
+  // print Polya-Vinogradov Bound
+  std::cerr << "Bounds" << std::endl;
+
+  std::cerr << "Polya-Vinogradov " << "\t" << n/2+NTL::SqrRoot(p)*NTL::log(p) << std::endl;
+  std::cerr << "Polya-Vinogradov (lower bound)" << "\t" << NTL::SqrRoot(p) << std::endl;
+  std::cerr << "Polya-Vinogradov (upper bound)" << "\t" << NTL::SqrRoot(p)*NTL::log(p) << std::endl;
+  std::cerr << "Montgomery-Vaughan" << "\t" << NTL::SqrRoot(p)*log(NTL::log(p)) << std::endl;
+  std::cerr << "Tao w/ Burgess Trick" << "\t" << NTL::SqrRoot(p)*NTL::log(p)*NTL::log(p) << std::endl;
+  std::cerr << "Schur (1918)" << "\t" << NTL::SqrRoot(p)/(2*PI) << std::endl;
   return 0;
 }
